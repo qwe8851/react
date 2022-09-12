47db541f0a746fc3da5b45eb34d3c4683c2dfaee
qwe8851 <dgh07027@naver.com>
2022-09-12 21:25:25 +0900

    map : div들을반복문으로 줄이기

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,12 +1,12 @@
 /* eslint-disable*/
 
 import './App.css';
-import { useState } from 'react';
+import { useState, useSyncExternalStore } from 'react';
 
 function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코드 추천', '강남 우동 맛집', '파이썬 독학']); 
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
 
 
@@ -35,19 +35,41 @@ function App() {
         글제목변경(copy);
       }}>글수정</button>
 
-
+{/* 
       <div className="list">
-        <h4>{ 글제목[0] } <span onClick={ () => {console.log( 따봉변경(따봉+1) )}}>👍</span> {따봉} </h4>
+        <h4 onClick={ ()=>{ setModal(!modal) } }>{ 글제목[0] } <span onClick={ () => {console.log( 따봉변경(따봉+1) )}}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 글제목[1] }</h4>
+        <h4 onClick={ ()=>{ setModal(!modal) } }>{ 글제목[1] }</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
         <h4 onClick={ ()=>{ setModal(!modal) } }>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
-      </div>     
+      </div>      */}
+
+
+
+
+      {
+        글제목.map(function(a, i){
+          return(
+            <div className="list" key={i}>
+            <h4 onClick={ ()=>{ setModal(!modal) } }>{ a } 
+              <span onClick={ () => {
+                let copy = [...따봉];
+                copy[i] += 1;
+                따봉변경(copy)  
+             }}>👍</span> {따봉[i]} 
+            </h4>
+            <p>2월 17일 발행</p>
+          </div>   
+          )     
+        })
+      }
+
+
 
       {
         modal == true ? <Modal/> : null
@@ -60,14 +82,11 @@ function App() {
   // Modal 컴포넌트 생성
   function Modal() {
     return(
-      <>
         <div className='model'>
           <h4>제목</h4>
           <p>날짜</p>
           <p>상세내용</p>
         </div>
-        <div></div>
-      </>
     )        
   }
 }
