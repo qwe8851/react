5e8cd9f2a6b73a4cbc0879935424e028d8ac940f
qwe8851 <dgh07027@naver.com>
2022-09-12 16:17:01 +0900

    모달창 만들기(제목 클릭 시 모달창 띄우기)

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,6 +7,7 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자 코드 추천', '강남 우동 맛집', '파이썬 독학']); 
   let [따봉, 따봉변경] = useState(0);
+  let [modal, setModal] = useState(false);
 
 
   return (
@@ -38,20 +39,19 @@ function App() {
       <div className="list">
         <h4>{ 글제목[0] } <span onClick={ () => {console.log( 따봉변경(따봉+1) )}}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
-        <hr />
       </div>
       <div className="list">
         <h4>{ 글제목[1] }</h4>
         <p>2월 17일 발행</p>
-        <hr />
       </div>
       <div className="list">
-        <h4>{ 글제목[2] }</h4>
+        <h4 onClick={()=>{setModal(true)}}>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
-        <hr />
       </div>     
 
-      <Modal></Modal>
+      {
+        modal == true ? <Modal/> : null
+      }
       
     </div>
   );
