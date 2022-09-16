a17177c83641920045e1d946a89a69d4d4ee54c7
qwe8851 <dgh07027@naver.com>
2022-09-16 23:13:09 +0900

    props2

--- blog/src/App.js
+++ blog/src/App.js
@@ -8,12 +8,10 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코드 추천', '강남 우동 맛집', '파이썬 독학']); 
   let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
 
 
   return (
-    // jsx문법1) class(x) className(o) 
-    // jsx문법2) 중괄호 문법 : {변수명}을 이용하여 데이터 바인딩 가능
-    // jsx문법3) style을 넣을때는 object형식으로 (font-size 같은 경우는 fontSize로)
 
     <div className="App">
       <div className="black-nav">
@@ -34,28 +32,12 @@ function App() {
         글제목변경(copy);
       }}>글수정</button>
 
-{/* 
-      <div className="list">
-        <h4 onClick={ ()=>{ setModal(!modal) } }>{ 글제목[0] } <span onClick={ () => {console.log( 따봉변경(따봉+1) )}}>👍</span> {따봉} </h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4 onClick={ ()=>{ setModal(!modal) } }>{ 글제목[1] }</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4 onClick={ ()=>{ setModal(!modal) } }>{ 글제목[2] }</h4>
-        <p>2월 17일 발행</p>
-      </div>      */}
-
-
-
 
       {
         글제목.map(function(a, i){
           return(
             <div className="list" key={i}>
-            <h4 onClick={ ()=>{ setModal(!modal) } }>{ a } 
+            <h4 onClick={ ()=>{ setModal(!modal); setTitle(i)} }>{ a } 
               <span onClick={ () => {
                 let copy = [...따봉];
                 copy[i] += 1;
@@ -69,9 +51,8 @@ function App() {
       }
 
 
-
       {
-        modal == true ? <Modal 글제목={글제목} /> : null
+        modal == true ? <Modal title={title} 글제목={글제목} /> : null
       }
       
     </div>
@@ -82,9 +63,10 @@ function App() {
   function Modal(props) {
     return(
         <div className='model'>
-          <h4>{props.글제목[0]}</h4>
+          <h4>{props.글제목[props.title]}</h4>
           <p>날짜</p>
           <p>상세내용</p>
+          <button onClick={()=> 글제목변경()}>글 수정</button>
         </div>
     )        
   }
