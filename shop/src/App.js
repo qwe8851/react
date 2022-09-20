915f1bd21bb9f781d60d9872e3890408c21e0247
qwe8851 <dgh07027@naver.com>
2022-09-20 23:55:42 +0900

    라우팅

--- shop/src/App.js
+++ shop/src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { Navbar, Container, Nav,Row, Col } from 'react-bootstrap';
 import './App.css';
 import data from './data.js';
-
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 
@@ -11,38 +11,39 @@ function App() {
   let [shoes, setShoes] = useState(data);
 
   return (    
-    <div className="App">
+    <div className="App">  
 
       {/* navbar */}
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link to="/">홈</Nav.Link>
+            <Link to="/detail">상세페이지</Link>
+            {/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
           </Nav>
         </Container>
       </Navbar>
-
-    {/* 백그라운드 이미지 삽입 */}
-      <div className="main-bg"></div>
       
-      <Container>
-        <Row>
-          {/* shoes를 기준으로 도는 map */}
-          {
-            shoes.map((a,i) => {
-              return(
-                // <Card shoes={shoes[i]} i={i}></Card>  
-              )
-            })
-          }
-        </Row>
-      </Container>
-   
-    </div>
+      {/* <Link to="/">홈</Link> */}
 
+      <Routes>
+        <Route path="/" element={
+          <>
+          <div className="main-bg"></div>
+          <Container>
+            <Row>
+              {  shoes.map((a,i) => {
+                  return <Card shoes={shoes[i]} i={i}></Card> 
+              })}
+            </Row>
+          </Container>
+          </>
+        } />
+        <Route path="/detail" element={<div>상세페이지임</div>} />
+        <Route path="/about" element={<div>어바웃페이지</div>}/>
+      </Routes>
+      </div>
   );
 }
 
