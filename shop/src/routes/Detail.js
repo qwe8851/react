4b0157090fad46a663571bae4bdae4303e13ba36
qwe8851 <dgh07027@naver.com>
2022-10-03 20:47:51 +0900

    styled-components

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,13 +1,40 @@
+/* eslint-disable */
+
+import { useEffect, useState } from "react";
 import{ useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+let Box = styled.div`
+    padding : 20px;
+    color : grey;
+`;
+
+let YellowBtn = styled.button`
+  background : ${ props => props.bg };
+  color : ${ props => props.bg == 'blue' ? 'white' : 'black' };
+  padding : 10px;
+`;
 
 function Detail(props) {
 
+    return(
+        <Box>
+            <YellowBtn bg="orange">버튼</YellowBtn>
+            <YellowBtn bg="blue">버튼</YellowBtn>
+        </Box>
+    )
+    useEffect(()=>{
+        console.log('안녕');
+    })
+
     let {id} = useParams();
     let findProduct = props.shoes.find((x)=>x.id == id);
+    let [count, setCount] = useState(0);
 
     return (
         <div className="container">
-            <div className="row">
+            <button onClick ={()=>{setCount(count+1)}}>버튼</button>
+            <div className="setCount">
                 <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
                 </div>
@@ -21,5 +48,7 @@ function Detail(props) {
         </div>
     );
 }
+
+
 
 export default Detail;
