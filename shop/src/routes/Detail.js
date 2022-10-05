93630c4ef92aac85911e8b6e3fd9e55f294310f0
qwe8851 <dgh07027@naver.com>
2022-10-05 23:38:22 +0900

    react로 tab UI 생성하기

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 
 import { useEffect, useState } from "react";
+import { Nav } from "react-bootstrap";
 import{ useParams } from "react-router-dom";
 
 
@@ -10,6 +11,7 @@ function Detail(props) {
     let findProduct = props.shoes.find((x)=>x.id == id);
     let [count, setCount] = useState(0);
     let [alert, setAlert] = useState(true);
+    let [tab, setTab] = useState(0);
 
     useEffect(()=>{
         setTimeout(() => {
@@ -35,9 +37,31 @@ function Detail(props) {
                     <button className="btn btn-danger">주문하기</button>
                 </div>
             </div>
+            
+            {/* tab UI */}
+            <Nav variant="tabs"  defaultActiveKey="link0">
+                <Nav.Item>
+                    <Nav.Link onClick={()=>{ setTab(0)}} eventKey="link0">버튼0</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={()=>{ setTab(1)}} eventKey="link1">버튼1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={()=>{ setTab(2)}} eventKey="link2">버튼2</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            
+            {/* 컴포넌트 사용 */}
+            <TabContent tab={tab} />
         </div>
     );
 }
+
+function TabContent({tab}){
+    console.log({tab});
+    return [ <div>내용0</div>, <div>내용1</div>, <div>내용2</div>][{tab}.tab]
+    // 왜 tab.tab으로 해야 들어가는지는 좀 더 해봐야 할 듯 하다 .. 10/05
+  }
 
 
 
